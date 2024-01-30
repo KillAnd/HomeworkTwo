18164b668dab73a6a732897b57614ab93e7a8391
kila1 <kila14081996@gmail.com>
2024-01-30 22:46:14 +0300

    8 задача. 1 пункт.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        var oneBoxer = 78.2;
-        var twoBoxer = 82.7;
-        var b = twoBoxer - oneBoxer;
-        System.out.println("Разница между весом боксеров " + b + " кг.");
-        b = twoBoxer % oneBoxer;
-        System.out.println("Разница между весом боксеров " + b + " кг.");
+        var fullTime = 640;
+        var oneTimeWorker = 8;
+        var quantityWorker = fullTime / oneTimeWorker;
+        System.out.println("Всего работников в компании - " + quantityWorker + " человек.");
     }
 }

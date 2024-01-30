d17b0b261a0e750f994ba7ea64b3c9b0aebb1b02
kila1 <kila14081996@gmail.com>
2024-01-30 22:53:20 +0300

    8 задача. 2 пункт.

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,9 @@ public class Main {
         var oneTimeWorker = 8;
         var quantityWorker = fullTime / oneTimeWorker;
         System.out.println("Всего работников в компании - " + quantityWorker + " человек.");
+
+        quantityWorker = quantityWorker + 94;
+        fullTime = quantityWorker * oneTimeWorker;
+        System.out.println("Если в компании работает " + quantityWorker + " человека, то всего " + fullTime + " часов работы может быть поделено между сотрудниками");
     }
 }

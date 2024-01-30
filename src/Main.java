d13afc1924b34a33d40d9082e895fb1c79164b7c
kila1 <kila14081996@gmail.com>
2024-01-30 21:48:27 +0300

    4 задача. Friend.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-
-        dog = dog + 4; // 12.0
-        cat = cat + 4; // 7.6
-        paper = paper + 4; // 763793
-
-        dog = dog - 3.5; // 8.5
-        cat = cat - 1.6; // 6.0
-        paper = paper - 7639; // 756154
-
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }

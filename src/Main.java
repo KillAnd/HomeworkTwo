e35f202b80f901552fd7ea2d8d11df2f48961a20
kila1 <kila14081996@gmail.com>
2024-01-30 22:32:52 +0300

    7 задача. Боксеры (2способа).

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         var oneBoxer = 78.2;
         var twoBoxer = 82.7;
-        var a = oneBoxer + twoBoxer;
         var b = twoBoxer - oneBoxer;
-        System.out.println("Общий вес боксеров " + a + " кг.");
+        System.out.println("Разница между весом боксеров " + b + " кг.");
+        b = twoBoxer % oneBoxer;
         System.out.println("Разница между весом боксеров " + b + " кг.");
     }
 }

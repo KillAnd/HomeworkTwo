78af4b60c68ca0a5de712c80b66cfde1a06ac686
kila1 <kila14081996@gmail.com>
2024-01-30 21:42:39 +0300

    3 задача. Вычитание. Добавил комментарии.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,15 @@ public class Main {
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
-        dog = dog + 4;
-        cat = cat + 4;
-        paper = paper + 4;
+
+        dog = dog + 4; // 12.0
+        cat = cat + 4; // 7.6
+        paper = paper + 4; // 763793
+
+        dog = dog - 3.5; // 8.5
+        cat = cat - 1.6; // 6.0
+        paper = paper - 7639; // 756154
+
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
